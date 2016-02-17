9bc2c5e293a9a551fe71d96798efce90d6a8fd5e
Brian Manning <brian@xaoc.org>
2016-02-16 21:03:21 -0800

    src/main.rs: added HTML parser
    - DIV count with https://en.wikipedia.org: 78

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate hyper;
+#[macro_use]
+extern crate string_cache;
+extern crate html5ever;
+extern crate tendril;
+use hyper::Client;
+use tendril::stream::TendrilSink;
+
+fn count_divs(hdl: &html5ever::rcdom::Handle) -> isize {
+   use html5ever::rcdom::NodeEnum::Element;
+   let nodep = hdl.borrow();
+   let mut divs = 0;
+
+   if let Element(ref qname, _, _) = nodep.node {
+      if qname.local == atom!("div") {
+         divs += 1;
+         println!("{:?}", qname);
+      }
+   }
+   for child in &nodep.children {
+      divs += count_divs(child);
+   }
+   divs
+}
+
 fn main() {
-    println!("Hello, world!");
+   //println!("Hello, world!");
+   let client = Client::new();
+
+   let mut res = client.get("https://en.wikipedia.org").send().unwrap();
+   assert_eq!(res.status, hyper::Ok);
+   let dom = html5ever::rcdom::RcDom::default();
+   let parser = html5ever::parse_document(dom, html5ever::ParseOpts::default());
+   let parserb = parser.from_bytes(html5ever::driver::BytesOpts::default());
+   let outp = parserb.read_from(&mut res).unwrap();
+   println!("{:?}", count_divs(&outp.document));
 }
